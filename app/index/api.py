2ca4f73fd93385dc812e69ebf73ce291c12adf52
wangzaogen <wangzaogen@111.com.cn>
2021-01-13 15:27:15 +0800

    to json

--- app/index/api.py
+++ app/index/api.py
@@ -1,7 +1,8 @@
 from app.index import index
 from flask import Flask, request,g, session, redirect, url_for, abort, \
     render_template, flash
-
+import json
+import traceback
 
 # @index.route('/')  # 设置路由
 # def index():  # 执行的方法
@@ -25,3 +26,25 @@ def add_entry():
     g.db.commit()
     flash('New entry was successfully posted')
     return redirect(url_for('index.show_entries'))
+
+@index.route('/to_json', methods=['GET', 'POST'])  # 设置路由
+def to_json():  # 执行的方法
+    result = None
+    if request.method == 'POST':
+        try:
+            json_str  = request.form['json_str']
+            print(json_str)
+            json_object = json.loads(json_str)
+            result = json.dumps(json_object, sort_keys=True, indent=4, separators=(',', ': '))
+            print(result)
+        except Exception as e:
+            traceback.print_exc()
+            return render_template('json_fmt.html',error = "RangeError: Invalid array length")
+
+    return render_template('json_fmt.html',result = result)
+
+if __name__ == '__main__':
+    str = '{"a": "Runoob", "b": 7}'
+    json_object = json.loads(str)
+    json_str = json.dumps(json_object, sort_keys=True, indent=4, separators=(',', ': '))
+    print(json_str)
